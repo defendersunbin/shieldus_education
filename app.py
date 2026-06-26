aa9e255b1005b22f505256a46e44d74b1315b38f
defendersunbin <defendersunbin98@naver.com>
2026-06-26 15:08:23 +0900

    save

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 #모듈: 외부에 있는 모듈을 사용
 # import mod1
+# import mod1 as md
+# from mod1 import PI, add, sub
+from mod1 import *
+# from mod.mod2 import *
+
+print('__name__ :', __name__)
+
+print(PI)
+print(add(10, 2))
+print(sub(5, 2))
+print(sub(2, 5))
+
+# print(md.PI)
+# print(md.add(6, 3))
+# print(md.sub(6, 4))
+# print(md.sub(4, 5))
 
 # print(mod1.PI)
 # print(mod1.add(5, 2))
 # print(mod1.sub(5, 2))
 # print(mod1.sub(2, 5))
+
+def start():
+    print('start')
+
+if __name__ == '__main__':
+    start()
---
+++ mod/mod2.py
@@ -0,0 +1,13 @@
+#모듈: 변수들, 함수들, 클래스들 가지는 파일
+print('mod.mod2 실행 시작')
+
+PI = 3.14
+
+def add(n1, n2):
+    
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+print('mod.mod2 실행 끝')
